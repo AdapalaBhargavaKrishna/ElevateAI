d6eb8c53b955aa712cb04e4afbc4bc913c186176
AdapalaBhargavaKrishna <bk.adapala@gmail.com>
2026-03-03 18:03:25 +0530

    Starting Backend

---
+++ api-services/src/index.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+// app.use("/auth", authRoutes);
+
+app.get('/health', (req, res) => {
+    res.json({ status: 'ok' })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
